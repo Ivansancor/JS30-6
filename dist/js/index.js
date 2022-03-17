a8e3d1b3a9cc833fc66922691a5af65ac1de3e62
Ivansancor <ivanvaldeolivo95@gmail.com>
2022-03-17 15:04:37 +0100

    same lol

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,8 +1,5 @@
 const searchbar = document.querySelector("input[type=text]");
-const button = document.querySelector("input[type=button");
 const resultBlock = document.querySelector(".suggestions");
-
-searchbar.value = "";
 
 function filterResults() {
     resultBlock.innerHTML += `<li>${this.value}</li>`;
